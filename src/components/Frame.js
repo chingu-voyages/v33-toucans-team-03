d0e0335dbaa241e423e92df20f7f8f02840bd732
GUNEET SINGH <guneetsingh@GUNEETs-MacBook-Air.local>
2021-09-14 21:15:01 +0530

    Full Page review Added

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Frame(props) {
-    const html= props.html
-    const js= `<script>${props.js}</script> </body></html>`
-    const cs=`<html><head></head><style>${props.css}</style><body>`
+    const html = props.html;
+    const js = `<script>${props.js}</script> </body></html>`;
+    const cs = `<html><head></head><style>${props.css}</style><body>`;
     return (
         <div>
-            <iframe srcDoc={cs+html+js} title="Hey you will see the webpage here" height="700" width="800"></iframe>
+        
+            <button><Link to={props.location === "home" ? "/fullpage" : "/"} >{props.location === "home" ? "Full Screen":"Home screen"}</Link></button>
+            <iframe srcDoc={cs + html + js} title="Hey you will see the webpage here" height="700" width="800"></iframe>
         </div>
     )
 }
