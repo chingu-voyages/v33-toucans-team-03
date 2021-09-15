7427ff19fbcf275265052670f0d95d8713d9cb42
ArjunHollaM <67258488+ArjunHollaM@users.noreply.github.com>
2021-09-15 19:46:50 +0530

    Changes in responsiveness

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,29 +1,28 @@
-import React,{useState}  from "react";
+import React, { useState } from "react";
 
-const Editor = (props) =>  {
-    const [value,setValue] = useState("");
-    const [isOpen,setOpen] = useState(true);
-    const changeValue = (e) =>{
+const Editor = (props) => {
+    const [value, setValue] = useState("");
+    const [isOpen, setOpen] = useState(true);
+    const changeValue = (e) => {
         setValue(e.target.value)
         props.click(e)
     }
     const handleToggle = () => {
         setOpen(!isOpen);
     }
-    const eraseText = () => {
-       // document.getElementById(`${props.id}`).value = "";
-       setValue("");
+    const eraseText = (e) => {
+        setValue("");
+        props.click(e)
     }
-
     return (
-        <div className = "AllEditors">
-            <div className = "titles">{props.editorTitle}
-            <div>
-                <button id = "clr" onClick = {eraseText}>Clear</button>
-                <button onClick = {handleToggle}>{isOpen? "[-]":"[+]"}</button>
+        <div className="AllEditors">
+            <div className="titles">{props.editorTitle}
+                <div>
+                    <button id="clr" onClick={eraseText}>Clear</button>
+                    <button onClick={handleToggle}>{isOpen ? "[-]" : "[+]"}</button>
+                </div>
             </div>
-        </div>
-            <textarea className = {isOpen?"opened":"closed"} value={value} onChange={changeValue} row = "7" id = {props.id} placeholder = {props.placeholder}></textarea>
+            <textarea className={isOpen ? "opened" : "closed"} value={value} onChange={changeValue} row="7" id={props.id} placeholder={props.placeholder}></textarea>
         </div>
     );
 }
