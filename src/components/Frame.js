4a3f950cdd0f79777706533c379d29773ec228d5
GUNEET SINGH <guneetsingh@GUNEETs-MacBook-Air.local>
2021-09-20 21:03:58 +0530

    issue #20 solved

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -7,8 +7,8 @@ function Frame(props) {
     const cs = `<html><head></head><style>${props.css}</style><body>`;
     return (
         <div class = "CompleteFrame">
-            <button><Link to={props.location === "home" ? "/fullpage" : "/"} >{props.location === "home" ? "Full Screen":"Home screen"}</Link></button>
-            <iframe srcDoc={cs + html + js} title="Hey you will see the webpage here" height="700" width="800"></iframe>
+            {/* <button><Link to={props.location === "home" ? "/fullpage" : "/"} >{props.location === "home" ? "Full Screen":"Home screen"}</Link></button> */}
+            <iframe srcDoc={cs + html + js} title="Hey you will see the webpage here"></iframe>
         </div>
     )
 }
