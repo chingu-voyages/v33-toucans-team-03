3e95cb5ac77e2209d9c626b0678cb07d5d9265e7
ArjunHollaM <67258488+ArjunHollaM@users.noreply.github.com>
2021-09-21 14:59:27 +0530

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+
+
+const Footer = () => {
+    return (
+        <footer id="main-footer" class="grid">
+            <div>
+                <h3><i className = "fa fa-copyright"></i><i>2021ChinguPen</i></h3>
+                <h4>Terms & Conditions | Privacy Policy | About | Site Map | FAQs</h4>
+            </div>
+            <div className = "SML">Follow us on :
+				<i className="fa fa-facebook"></i>
+				<i className="fa fa-twitter"></i>
+				<i className="fa fa-youtube"></i>
+				<i className="fa fa-instagram"></i>	
+            </div>
+        </footer>
+    )
+}
+export default Footer;
